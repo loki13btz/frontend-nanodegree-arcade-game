9d527a38518c483d686bae073ca1fdb83a9979de
loki13btz <riotsoirit@gmail.com>
2022-09-05 23:25:51 +0300

    added Classic Frogger Game

--- js/app.js
+++ js/app.js
@@ -1,46 +1,102 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const cellWidth = 101;
+const cellHeigth = 83;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+
+// Enemies our player must avoid
+var Enemy = function (col, row) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = "images/enemy-bug.png";
+  this.x = col * cellWidth - 150;
+  this.y = row * cellHeigth - 25;
+  this.speed = Math.random() * (50) + 300;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Enemy.prototype.update = function (dt) {
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  if (this.x > 505){
+    this.x = -150;
+    this.y = parseInt(Math.random() * 3 + 1) * cellHeigth - 25;
+  }
+  this.x = this.x + dt * this.speed;
+  this.render();
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Enemy.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+const Player = function (col, row) {
+  this.sprite = "images/char-boy.png";
+  this.x = col * cellWidth;
+  this.y = row * cellHeigth - 25;
+};
+Player.prototype.update = function () {
+};
+Player.prototype.render = function (dt) {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function (input) {
+  switch (input) {
+    case "left":
+        if(this.x>0){
+            this.x -= cellWidth;
+        }
+      break;
+    case "up":
+        if(this.y>0){
+            this.y -= cellHeigth;
+        }
+      break;
+    case "right":
+        if(this.x<cellWidth*4){
+            this.x += cellWidth;
+        }
+      break;
+    case "down":
+        if(this.y<cellHeigth*4){
+            this.y += cellHeigth;
+        }
+      break;
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [new Enemy(1, 2), new Enemy(2, 3), new Enemy(3, 1)];
+const player = new Player(2,5);
 
-
+function checkCollisions(enemies,player) {
+    enemies.forEach(enemy => {
+    if((enemy.x - 50) < player.x && player.x < (enemy.x + 50) && enemy.y===player.y){
+        console.log([enemy.x + 5, player.x, enemy.x - 5]);
+        player.x = 2 * cellWidth;
+        player.y = 5 * cellHeigth - 25;
+    }
+});
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener("keyup", function (e) {
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
